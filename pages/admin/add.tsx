d0bbb47db35b260ce3ff7705e58ab319d45ef8c0
Mayska <>
2022-01-25 17:04:54 +0100

    ajout de add

---
+++ pages/admin/add.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import axios from 'axios';
+
+const Add: NextPage = () => {
+    return (<>
+        <br />
+        <h1 className='text-center'>Admin</h1>
+        <br />
+    </>)
+}
+
+export default Add
