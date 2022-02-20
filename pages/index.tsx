32cbdcfe46a1343e43929834cc5c1df765c0b35f
Mayska <>
2022-02-20 18:28:50 +0100

    ajout log error

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,22 @@ import { MyHome } from '../interfaces/myhome'
 
 function Index(props: any) {
   return (<>
+    {props.error != "" ? <p>props.error</p> : ""}
     <Home>{props}</Home>
   </>)
 }
 
 export async function getStaticProps() {
   const res: Response = await fetch(process.env.URL_API + '/home/category/home')
+  let error: string = ""
+  if (res.status != 200) {
+    error = "Error fetch => " + res.status
+  }
   const home: MyHome[] = await res.json()
   return {
     props: {
       home,
+      error
     },
   }
 };
