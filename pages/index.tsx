07daf84d011802df6e8dfed4970fadb6b9bc1180
Mayska <>
2022-02-25 11:31:59 +0100

    test fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,28 +3,25 @@ import Home from "../components/home"
 import { MyHome } from '../interfaces/myhome'
 
 
-function Index() {
-  const home: any = axios.get("https://nestradio.herokuapp.com" + '/home/category/home')
-    .catch(error => console.log(error))
-  console.log(home)
+function Index(props: any) {
+  console.log(props.home)
   return (<>
-    <h1>CUOCUCOUC</h1>
+    <h1>RERERERERRE</h1>
     {/*<Home>{home}</Home>*/}
   </>)
 }
 
 
-export default Index
 
-/*
 export async function getStaticProps() {
   const res: Response = await fetch("https://nestradio.herokuapp.com" + '/home/category/home')
   console.log(res)
   const home: MyHome[] = await res.json()
   return {
     props: {
-      home
-    },
+      home: home
+    }
   }
-};
-*/
+}
+
+export default Index
